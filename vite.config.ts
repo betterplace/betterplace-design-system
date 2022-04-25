bddce86eab803edf6eddd975bc98f8a16aa4a617
Josefie Zynga <josefie.zynga@betterplace.org>
2022-04-25 18:18:49 +0200

    remove duplicate import

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "node:path";
-import { defineConfig } from "vite";
 import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
