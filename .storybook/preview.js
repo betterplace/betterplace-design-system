10c25c94dd111e8c5b43ecfbd00fca4f41a515eb
Kamil Piwko <iz.podpolja@gmail.com>
2022-05-23 13:30:58 +0200

    update stub text

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,6 @@
 import { withDesign } from 'storybook-addon-designs'
-import { withThemeProvider, AVAILABLE_THEMES } from '../src/helpers'
+import { withThemeProvider } from '../src/helpers'
+import AVAILABLE_THEMES from '../src/lib/shared/themes'
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
--- src/lib/components/button/button.tsx
+++ src/lib/components/button/button.tsx
@@ -3,7 +3,7 @@ import { ButtonProps } from './types'
 import styles from './button.module.css'
 
 function Button({}: ButtonProps) {
-  return <div className={styles.buttonMain}></div>
+  return <div className={styles.buttonMain}>This is as Button stub</div>
 }
 
 export default Button
