f0b7d239b2a5f8efb0da835b9d88cfc6734ee9f0
inesczuia <ines.czuia@betterplace.org>
2022-06-14 12:22:56 +0200

    clean up of styles

--- .storybook/betterplace-theme.js
+++ .storybook/betterplace-theme.js
@@ -10,15 +10,9 @@ export default create({
 
   textColor: '#292929',
   fontBase: '"Fira Sans", sans-serif',
-  textSize: '20rem'
+
 
   colorPrimary: '#9ecb0a',
   colorSecondary: '#6d2c64',
 
-  appBorderRadius: 4,
-
-  // Toolbar default and active colors
-  barTextColor: '#292929',
-  barSelectedColor: 'black',
-  barBg: '#9ecb0a',
 })
