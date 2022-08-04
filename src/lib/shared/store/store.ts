ea04ca1b7802c86afa759d8bb3fa3844ad92052e
Kamil Piwko <iz.podpolja@gmail.com>
2022-08-04 14:13:23 +0200

    wip

--- src/lib/shared/store/store.ts
+++ src/lib/shared/store/store.ts
@@ -9,6 +9,8 @@ import {
   delay,
   observeOn,
   queueScheduler,
+  shareReplay,
+  Observable,
 } from 'rxjs'
 
 import { Action, Effect, Reducer } from './types'
@@ -18,31 +20,30 @@ class Store<S extends {}, A extends Action> implements Subscribable<S>, Observer
 
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   protected _error$: Subject<any> = new Subject()
-  protected _state$: BehaviorSubject<S>
+  protected _state$: Observable<S>
   protected _action$: Subject<A> = new Subject()
   protected _sideEffect$: Subject<[A, S, S]> = new Subject()
+  pipe: typeof this._state$.pipe
   constructor(initialState: S, reducer: Reducer<S, A>, effects: Array<Effect<S, A>>) {
     console.log('Store created', initialState)
-    this._state$ = new BehaviorSubject<S>(initialState)
+    this._state$ = this._action$.pipe(
+      observeOn(queueScheduler),
+      scan((state, action) => {
+        const newState = reducer(state, action)
+        this._sideEffect$.next([action, state, newState])
+        console.log(action, state, newState, state === newState)
+        return newState
+      }, initialState),
+      shareReplay(1)
+    )
     this.pipe = this._state$.pipe
-    this._action$
-      .pipe(
-        observeOn(queueScheduler),
-        scan((state, action) => {
-          const newState = reducer(state, action)
-          this._sideEffect$.next([action, state, newState])
-          console.log(action, state, newState, state === newState)
-          return newState
-        }, initialState)
-      )
-      .subscribe((state) => this._state$.next(state))
+
     merge(...effects.map((effect) => effect(this._sideEffect$)))
       .pipe(delay(33))
       .subscribe((action) => this._action$.next(action as A))
     this._action$.next({ type: '@@init', payload: undefined } as A)
   }
 
-  pipe: typeof this._state$.pipe
   subscribe(observer: Partial<Observer<S>>): Unsubscribable {
     return this._state$.subscribe(observer)
   }
