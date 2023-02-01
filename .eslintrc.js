3ac3a9be4b1b2a017acd41aa9c42425a700ae1bd
Josefie Zynga <josefie.zynga@betterplace.org>
2023-02-01 16:21:20 +0100

    automigrate to storybook 7

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,63 +4,55 @@ module.exports = {
   parserOptions: {
     project: ['./tsconfig.base.json', './tsconfig.tsnode.json'],
     files: ['src/**/*', 'config/**/*'],
-    extraFileExtensions: ['.md', '.mdx'],
+    extraFileExtensions: ['.md', '.mdx']
   },
   root: true,
   plugins: ['@typescript-eslint', 'react', 'prettier'],
   env: {
     browser: true,
     node: true,
-    es6: true,
+    es6: true
   },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react/recommended',
-    'plugin:react-hooks/recommended',
-    'plugin:prettier/recommended',
-  ],
-  overrides: [
-    {
-      files: ['src/**/*.mdx', 'src/**/*.md'],
-      extends: ['eslint:recommended', 'plugin:mdx/recommended', 'plugin:prettier/recommended'],
-      parser: 'eslint-mdx',
-    },
-    { files: ['figma/**/*', 'generator/**/*'], parserOptions: { project: './tsconfig.tsnode.json' } },
-  ],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react/recommended', 'plugin:react-hooks/recommended', 'plugin:prettier/recommended', 'plugin:storybook/recommended'],
+  overrides: [{
+    files: ['src/**/*.mdx', 'src/**/*.md'],
+    extends: ['eslint:recommended', 'plugin:mdx/recommended', 'plugin:prettier/recommended'],
+    parser: 'eslint-mdx'
+  }, {
+    files: ['figma/**/*', 'generator/**/*'],
+    parserOptions: {
+      project: './tsconfig.tsnode.json'
+    }
+  }],
   settings: {
     react: {
-      version: 'detect',
-    },
+      version: 'detect'
+    }
   },
   rules: {
     'import/no-unassigned-import': 0,
     'import/no-named-as-default-member': 0,
     'import/order': 0,
     'react/prop-types': 0,
-    'no-empty': ['error', { allowEmptyCatch: true }],
+    'no-empty': ['error', {
+      allowEmptyCatch: true
+    }],
     '@typescript-eslint/explicit-module-boundary-types': 0,
-    '@typescript-eslint/ban-types': [
-      'error',
-      {
-        extendDefaults: true,
-        types: {
-          '{}': false,
-        },
-      },
-    ],
-    '@typescript-eslint/no-unused-vars': [
-      'error',
-      {
-        vars: 'all',
-        args: 'all',
-        ignoreRestSiblings: true,
-        varsIgnorePattern: '^[_A-Z]',
-        argsIgnorePattern: '^_*',
-        caughtErrors: 'none',
-      },
-    ],
+    '@typescript-eslint/ban-types': ['error', {
+      extendDefaults: true,
+      types: {
+        '{}': false
+      }
+    }],
+    '@typescript-eslint/no-unused-vars': ['error', {
+      vars: 'all',
+      args: 'all',
+      ignoreRestSiblings: true,
+      varsIgnorePattern: '^[_A-Z]',
+      argsIgnorePattern: '^_*',
+      caughtErrors: 'none'
+    }],
     'react/jsx-uses-react': 'off',
-    'react/react-in-jsx-scope': 'off',
-  },
-}
+    'react/react-in-jsx-scope': 'off'
+  }
+};
--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,35 +1,35 @@
-import { Options } from '@storybook/core-common'
-import { StorybookConfig } from '@storybook/react/types'
-
-import { UserConfig } from 'vite'
-
+import { Options } from '@storybook/core-common';
+import { StorybookConfig } from '@storybook/react/types';
+import { UserConfig } from 'vite';
 interface ExtendedConfig extends StorybookConfig {
-  viteFinal?: (config: UserConfig, options: Options) => Promise<UserConfig>
+  viteFinal?: (config: UserConfig, options: Options) => Promise<UserConfig>;
 }
 const config: ExtendedConfig = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
-    '@storybook/addon-jest',
-    '@storybook/addon-a11y',
-    'storybook-addon-designs',
-  ],
-  framework: '@storybook/react',
-  reactOptions: { strictMode: true },
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions', '@storybook/addon-jest', '@storybook/addon-a11y', 'storybook-addon-designs'],
+  framework: {
+    name: '@storybook/react-vite',
+    options: {
+      strictMode: true
+    }
+  },
+  reactOptions: {
+    strictMode: true
+  },
   core: {
-    builder: '@storybook/builder-vite',
+    builder: '@storybook/builder-vite'
   },
   typescript: {
-    reactDocgen: 'react-docgen-typescript',
+    reactDocgen: 'react-docgen-typescript'
   },
   async viteFinal(config) {
-    console.log('env', process.env.BASE_URL, process.env.STORYBOOK_FIGMA_ACCESS_TOKEN)
-    config.base = process.env.BASE_URL || config.base
+    console.log('env', process.env.BASE_URL, process.env.STORYBOOK_FIGMA_ACCESS_TOKEN);
+    config.base = process.env.BASE_URL || config.base;
     // return the customized config
-    return config
+    return config;
   },
-}
-
-export default config
+  docs: {
+    autodocs: true
+  }
+};
+export default config;
